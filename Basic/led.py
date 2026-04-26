a33f925891aa7c99823784fe364e56eb5ccc5d2d
ShreeyashBihade <145752472+ShreeyashBihade@users.noreply.github.com>
2026-04-26 12:36:01 +0530

    Add LED control script for Raspberry Pi

---
+++ Basic/led.py
@@ -0,0 +1,21 @@
+import RPi.GPIO as GPIO
+import time
+
+LED_PIN = 2
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(LED_PIN, GPIO.OUT)
+
+print("CTRL+C to stop")
+
+try:
+        while True:
+                GPIO.output(LED_PIN, GPIO.HIGH)
+                time.sleep(1)
+                GPIO.output(LED_PIN, GPIO.LOW)
+                time.sleep(1)
+except KeyboardInterrupt:
+        print("Program Stopped")
+finally:
+        GPIO.cleanup()
+        print("GPIO Released")
